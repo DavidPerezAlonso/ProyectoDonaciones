815c2724631e88f8c794fca80dac14edebbb580d
DavidPerezAlonso <david.perez@gmail.com>
2020-04-30 09:32:14 +0200

    Commit inicial: árbol del proyecto con MVC y la vista principal operativa

---
+++ src/Vista/ControlUIMenuPrincipal.java
@@ -0,0 +1,41 @@
+package Vista;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControlUIMenuPrincipal {
+
+	@FXML
+	private Button btn_donantes;
+
+	@FXML
+	private Button btn_donaciones;
+
+	@FXML
+	private Button btn_informes;
+
+
+	public void Donantes(){
+
+		// Aquí el código para abrir pantalla Donantes
+		System.out.println("Donantes");
+
+	}
+
+
+	public void Donaciones(){
+
+		// Aquí el código para abrir pantalla Donaciones
+		System.out.println("Donaciones");
+
+	}
+
+
+	public void Informes(){
+
+		// Aquí el código para abrir pantalla Informes
+		System.out.println("Informes");
+
+	}
+
+}
